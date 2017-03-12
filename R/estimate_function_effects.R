9ee1bfaadd637ee4f05ab39f1bb0c2a86da6ef7c
sw1 <stephenwoloszynek@gmail.com>
2017-03-12 15:04:03 -0400

    added return and header doc

--- R/estimate_function_effects.R
+++ R/estimate_function_effects.R
@@ -1,3 +1,23 @@
+#' Estimate the predicted function-topic effects
+#'
+#' Given within topic functional predictions, estimate the effects at a given
+#' gene function category level. The effects correspond to a topic-gene category
+#' interaction term after accounting for topic and gene category effects. The
+#' model can be fit via either maximum likelihood or Hamiltonian MC -- see
+#' details.
+#'
+#' @param functions List containing the output of
+#'   \code{\link{predict_functions}}.
+#' @param topics_subset Vector of intergers corresponding to a subset of topics
+#'   to be evaluated. Recommended to be < 25.
+#' @param level (optional) The gene category level to evalulate. Defaults to 2.
+#' @param method (optional) String indicating either ML or HMC. Defaults to ML.
+#' @param verbose (optional) Logical to print progress. Defaults to TRUE.
+#'
+#' @return A glmerMod object (ML) or a list containing rstan output
+#'   (HMC).
+#' @export
+
 estimate_function_effects <- function(functions,topics_subset,level=2,method='ML',verbose=FALSE){
 
   fxn_table <- functions$fxn_table
@@ -14,14 +34,18 @@ estimate_function_effects <- function(functions,topics_subset,level=2,method='ML
   gene_table <- format_gene_table(functions,level=level)
 
   if (method == 'ML'){
+
     mm <- lme4::glmer.nb(count ~ (1|pw) + (1|topic) + (1|pw:topic),
                          data=gene_table,
                          verbose=verbose)
+
   }
   if (method == 'HMC'){
 
     mm <- fit_stan_model(gene_table,iters=1000,chains=4)
 
   }
+
+  return(mm)
 
 }
