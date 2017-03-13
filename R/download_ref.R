d0fc2d130cd95a27413edf5fa559eb345a616f8f
sw1 <stephenwoloszynek@gmail.com>
2017-03-13 00:43:16 -0400

    added tax4fun

--- R/download_ref.R
+++ R/download_ref.R
@@ -8,21 +8,23 @@
 #' @param verbose Logical to print status of download.
 #' @export
 
-download_ref <- function(reference,destination,verbose=FALSE){
+download_ref <- function(reference='all',destination,verbose=FALSE){
 
   dir.create(destination,showWarnings=FALSE,recursive=TRUE)
 
-  if (reference == 'all'){
-    ref <- c('ko','cog')
-  }else if (reference == 'ko'){
-    ref <- 'ko'
-  }else if (reference == 'cog'){
-    ref <- 'cog'
+  ref <- c('gg_ko','gg_cog','silva_ko')
+
+  fns <- c('ko_13_5_precalculated.tab.gz',
+           'cog_13_5_precalculated.tab.gz',
+           't4f_ref_profiles.rds')
+
+  if (reference != 'all'){
+    fns <- fns[ref %in% reference]
   }
 
-  for (r in ref){
-    download.file(sprintf('https://github.com/sw1/themetagenomics_data/raw/master/%s_13_5_precalculated.tab.gz',r),
-                  destfile(file.path(destination,sprintf('%s_13_5_precalculated.tab.gz',r))),
+  for (fn in fns){
+    download.file(sprintf('https://github.com/sw1/themetagenomics_data/raw/master/%s',fn),
+                  destfile(file.path(destination,fn)),
                   quiet=verbose,
                   mode='wb')
   }
