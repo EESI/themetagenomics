34c6c161c9ad1e1805fd55fdf295c7e1236451a2
sw1 <stephenwoloszynek@gmail.com>
2017-03-14 18:46:28 -0400

    added wrapper for glmer.nb

--- R/estimate_function_effects.R
+++ R/estimate_function_effects.R
@@ -18,10 +18,14 @@
 #'   (HMC).
 #' @export
 
-estimate_function_effects <- function(functions,topics_subset,level=2,method='ML',verbose=FALSE){
+estimate_function_effects <- function(functions,topics_subset,level=2,method='ML',...){
 
   fxn_table <- functions$fxn_table
   fxn_meta <- functions$fxn_meta
+
+  if (missing(topics_subset) & nrow(fxn_table) <= 25){
+    topics_subset <- 1:nrow(fxn_table)
+  }
 
   fxn_table <- fxn_table[topics_subset,]
   fxn_table <- fxn_table[,colSums(fxn_table)>0]
@@ -35,17 +39,15 @@ estimate_function_effects <- function(functions,topics_subset,level=2,method='ML
 
   if (method == 'ML'){
 
-    mm <- lme4::glmer.nb(count ~ (1|pw) + (1|topic) + (1|pw:topic),
-                         data=gene_table,
-                         verbose=verbose)
+    mm <- fit_ml_model(gene_table,...)
 
   }
   if (method == 'HMC'){
 
-    mm <- fit_stan_model(gene_table,iters=1000,chains=4)
+    mm <- fit_stan_model(gene_table,...)
 
   }
 
-  return(mm)
+  return(list(model=mm,gene_table=gene_table))
 
 }
