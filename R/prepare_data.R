cdce33af035d194673337e35e6b0171c29084aba
sw1 <stephenwoloszynek@gmail.com>
2017-03-31 02:50:44 -0400

    fixed it bugging out for raw otu table only

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -73,8 +73,9 @@ prepare_data <- function(otu_table,rows_are_taxa,tax_table,metadata,formula,refs
                 refs=NULL,
                 splineinfo=NULL,
                 modelframe=NULL)
-  class(slots) <- 'themetadata'
   refs_type <- NULL
+  splines <- NULL
+  class(slots) <- 'themetadata'
 
   if (rows_are_taxa) otu_table <- t(otu_table)
 
@@ -88,79 +89,104 @@ prepare_data <- function(otu_table,rows_are_taxa,tax_table,metadata,formula,refs
     if (miss$metadata)
       stop('Must provide metadata if a formula is given.')
 
-  if (miss$formula){
-    formula <- ~1
-    refs_type <- 'none'
-  }
-  if (miss$metadata){
-    if (rows_are_taxa){
-      metadata <- matrix(1,nrow=ncol(otu_table),2,dimnames=list(colnames(otu_table),NULL))
-    }else{
-      metadata <- matrix(1,nrow=nrow(otu_table),2,dimnames=list(rownames(otu_table),NULL))
+  # reorient tax_table and metadata
+  if (!miss$metadata){
+    if (verbose) cat('Checking row and column names.\n')
+    if (sum(rownames(otu_table) %in% colnames(metadata)) > sum(rownames(otu_table) %in% rownames(metadata))){
+      if (verbose) cat('Transposing metadata to reorient sample IDs.\n')
+      metadata <- t(metadata)
     }
   }
-  if (miss$tax_table){
-    if (rows_are_taxa){
-      tax_table <- matrix(NA,nrow(otu_table),7,dimnames=list(rownames(otu_table),NULL))
-    }else{
-      tax_table <- matrix(NA,ncol(otu_table),7,dimnames=list(colnames(otu_table),NULL))
+  if (!miss$tax_table & !miss$metadata)
+    if (sum(colnames(otu_table) %in% colnames(tax_table)) > sum(colnames(otu_table) %in% rownames(metadata))){
+      if (verbose) cat('Transposing tax_table to reorient taxa IDs.\n')
+      tax_table <- t(tax_table)
     }
-  }
 
-  if (verbose) cat('Checking row and column names.\n')
-  if (sum(rownames(otu_table) %in% colnames(metadata)) > sum(rownames(otu_table) %in% rownames(metadata))){
-    if (verbose) cat('Transposing metadata to reorient sample IDs.\n')
-    metadata <- t(metadata)
-  }
-  if (sum(colnames(otu_table) %in% colnames(tax_table)) > sum(colnames(otu_table) %in% rownames(metadata))){
-    if (verbose) cat('Transposing tax_table to reorient taxa IDs.\n')
-    tax_table <- t(tax_table)
+  # if unsupervised
+  if (miss$formula){
+    if(is.null(dimnames(otu_table))){
+      rownames(otu_table) <- paste0('s',seq_len(nrow(otu_table)))
+      colnames(otu_table) <- paste0('f',seq_len(ncol(otu_table)))
+      cn_normalize <- FALSE
+    }
+    if (cn_normalize){
+      if (verbose)  cat('Performing copy number normalization.\n')
+      otu_table <- cnn(otu_table,FALSE,drop=FALSE)
+    }
+    if (drop){
+      otu_table <- otu_table[,colSums(otu_table) > 0]
+      otu_table <- otu_table[rowSums(otu_table) > 0,]
+    }
+    if (!miss$metadata){
+      intersection <- intersect(rownames(otu_table),rownames(metadata))
+      otu_table <- otu_table[intersection,]
+      metadata <- metadata[intersection,,drop=FALSE]
+      slots$metadata <- metadata
+    }
+    if (!miss$tax_table){
+      intersection <- intersect(colnames(otu_table),rownames(tax_table))
+      otu_table <- otu_table[,intersection]
+      tax_table <- tax_table[intersection,]
+      slots$tax_table <- tax_table
+    }
+    slots$otu_table <- otu_table
+
+    attr(slots,'splines') <- splines
+    attr(slots,'refs') <- refs_type
+    attr(slots,'cnn') <- cn_normalize
+    attr(slots,'drop') <- drop
+
+    return(slots)
   }
 
   classes <- sapply(metadata,class)
-  if (miss$metadata) classes_counts <- c('n'=0,'c'=0,'f'=0) else classes_counts <- c('n'=sum(classes=='numeric'),'c'=sum(classes=='character'),'f'=sum(classes=='factor'))
+  classes_counts <- c('n'=sum(classes=='numeric'),
+                      'c'=sum(classes=='character'),
+                      'f'=sum(classes=='factor'))
   if (verbose) cat(sprintf('\nStarting stats:
               N otu_table samples: %s
               N otu_table taxa: %s\n
               N metadata numeric %s
               N metadata character %s
-              N metadata factor %s\n
+              N metadata factor %s\n',
+                           nrow(otu_table),
+                           ncol(otu_table),
+                           classes_counts['n'],
+                           classes_counts['c'],
+                           classes_counts['f']))
+  if (!miss$tax_table)
+    if (verbose) cat(sprintf('
               N phyla: %s
               N classes: %s
               N orders: %s
               N families: %s
               N genera: %s
               N species: %s\n\n',
-              nrow(otu_table),
-              ncol(otu_table),
-              classes_counts['n'],
-              classes_counts['c'],
-              classes_counts['f'],
-              length(na.omit(unique(tax_table[,2]))),
-              length(na.omit(unique(tax_table[,3]))),
-              length(na.omit(unique(tax_table[,4]))),
-              length(na.omit(unique(tax_table[,5]))),
-              length(na.omit(unique(tax_table[,6]))),
-              length(na.omit(unique(tax_table[,7])))))
-
+                             length(na.omit(unique(tax_table[,2]))),
+                             length(na.omit(unique(tax_table[,3]))),
+                             length(na.omit(unique(tax_table[,4]))),
+                             length(na.omit(unique(tax_table[,5]))),
+                             length(na.omit(unique(tax_table[,6]))),
+                             length(na.omit(unique(tax_table[,7])))))
 
   splines <- check_for_splines(formula,metadata)
   if (splines) formula_tmp <- extract_spline_info(formula,metadata,remove_only=TRUE) else formula_tmp <- formula
-  if (!miss$metadata){
-    if (verbose) if (any(is.na(metadata))) cat('Removing NA values in metadata.\n')
-    metadata <- model.frame(formula_tmp,data=metadata,na.action=na.omit)
-  }
+
+  if (verbose) if (any(is.na(metadata))) cat('Removing NA values in metadata.\n')
+  metadata <- model.frame(formula_tmp,data=metadata,na.action=na.omit)
 
   intersection <- intersect(rownames(otu_table),rownames(metadata))
   otu_table <- otu_table[intersection,]
   metadata <- metadata[intersection,,drop=FALSE]
 
-  intersection <- intersect(colnames(otu_table),rownames(tax_table))
-  otu_table <- otu_table[,intersection]
-  tax_table <- tax_table[intersection,]
-
+  if (!miss$tax_table){
+    intersection <- intersect(colnames(otu_table),rownames(tax_table))
+    otu_table <- otu_table[,intersection]
+    tax_table <- tax_table[intersection,]
+    if (any(is.na(tax_table))) stop('NA values in tax_table. Please correct.\n')
+  }
   if (any(is.na(otu_table))) stop('NA values in otu_table. Please correct.\n')
-  if (!miss$tax_table) if (any(is.na(tax_table))) stop('NA values in tax_table. Please correct.\n')
 
   if (cn_normalize){
     if (verbose)  cat('Performing copy number normalization.\n')
@@ -171,7 +197,7 @@ prepare_data <- function(otu_table,rows_are_taxa,tax_table,metadata,formula,refs
     otu_table <- otu_table[,colSums(otu_table) > 0]
     otu_table <- otu_table[rowSums(otu_table) > 0,]
     metadata <- metadata[rownames(otu_table),,drop=FALSE]
-    tax_table <- tax_table[colnames(otu_table),]
+    if (!miss$tax_table) tax_table <- tax_table[colnames(otu_table),]
   }
 
   classes <- sapply(metadata,class)
@@ -181,12 +207,16 @@ prepare_data <- function(otu_table,rows_are_taxa,tax_table,metadata,formula,refs
     metadata <- as.data.frame(unclass(metadata))
     rownames(metadata) <- rnames
   }
+
   classes <- sapply(metadata,class)
+  # manage factor references
   if (sum(classes == 'factor') > 0){
+    # is missing, set as level 1
     if (miss$refs){
       warning('References are recommended for factors. Using the first level(s).')
       refs_type <- 'level_1'
       refs <- unlist(lapply(metadata[,classes == 'factor'],function(x) levels(x)[1]))
+    # otherwise, set per user specifications
     }else{
       refs_type <- 'user'
       if (sum(classes == 'factor') != length(refs)) stop('A reference is required for each factor.')
@@ -201,52 +231,54 @@ prepare_data <- function(otu_table,rows_are_taxa,tax_table,metadata,formula,refs
           j <- j+1
         }
       }
-
-      if (splines){
-        splineinfo <- extract_spline_info(formula,metadata)
-        modelframe <- create_modelframe(splineinfo$formula,refs,metadata)
-        slots$splineinfo <- splineinfo
-      }else{
-        modelframe <- create_modelframe(formula,refs,metadata)
-      }
     }
+  }
+
+  if (splines){
+    splineinfo <- extract_spline_info(formula,metadata)
+    modelframe <- create_modelframe(splineinfo$formula,refs,metadata)
+    slots$splineinfo <- splineinfo
   }else{
     modelframe <- create_modelframe(formula,refs,metadata)
   }
   rownames(metadata) <- rownames(otu_table)
 
   classes <- sapply(metadata,class)
-  if (miss$metadata) classes_counts <- c('n'=0,'c'=0,'f'=0) else classes_counts <- c('n'=sum(classes=='numeric'),'c'=sum(classes=='character'),'f'=sum(classes=='factor'))
+  classes_counts <- c('n'=sum(classes=='numeric'),
+                      'c'=sum(classes=='character'),
+                      'f'=sum(classes=='factor'))
   if (verbose) cat(sprintf('\nFinal stats:
               N otu_table samples: %s
               N otu_table taxa: %s\n
               N metadata numeric %s
               N metadata character %s
-              N metadata factor %s\n
+              N metadata factor %s\n',
+                           nrow(otu_table),
+                           ncol(otu_table),
+                           classes_counts['n'],
+                           classes_counts['c'],
+                           classes_counts['f']))
+  if (!miss$tax_table)
+    if (verbose) cat(sprintf('
               N phyla: %s
               N classes: %s
               N orders: %s
               N families: %s
               N genera: %s
               N species: %s\n\n',
-              nrow(otu_table),
-              ncol(otu_table),
-              classes_counts['n'],
-              classes_counts['c'],
-              classes_counts['f'],
-              length(na.omit(unique(tax_table[,2]))),
-              length(na.omit(unique(tax_table[,3]))),
-              length(na.omit(unique(tax_table[,4]))),
-              length(na.omit(unique(tax_table[,5]))),
-              length(na.omit(unique(tax_table[,6]))),
-              length(na.omit(unique(tax_table[,7])))))
+                             length(na.omit(unique(tax_table[,2]))),
+                             length(na.omit(unique(tax_table[,3]))),
+                             length(na.omit(unique(tax_table[,4]))),
+                             length(na.omit(unique(tax_table[,5]))),
+                             length(na.omit(unique(tax_table[,6]))),
+                             length(na.omit(unique(tax_table[,7])))))
 
   slots$otu_table <- otu_table
-  slots$modelframe <- modelframe
   if (!miss$tax_table) slots$tax_table <- tax_table
-  if (!miss$metadata) slots$metadata <- metadata
-  if (!miss$refs) slots$refs <- refs
-  if (!miss$formula) slots$formula <- formula
+  slots$metadata <- metadata
+  slots$refs <- refs
+  slots$formula <- formula
+  slots$modelframe <- modelframe
 
   attr(slots,'splines') <- splines
   attr(slots,'refs') <- refs_type
