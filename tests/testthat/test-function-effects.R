ea0123bcd3e6febfbc6cca50b913cf258ed30bd9
sw1 <stephenwoloszynek@gmail.com>
2017-04-03 00:14:15 -0400

    babysteps -- adding rstan

--- tests/testthat/test-function-effects.R
+++ tests/testthat/test-function-effects.R
@@ -3,5 +3,9 @@ context("test function effects")
 test_that('picrust results gives similar results for ml and hmc',{
 
   expect_warning(est(GEVERS$functions,level=2,iters=5,method='ml'))
+  expect_warning(est(GEVERS$functions,level=2,iters=50,chains=1,
+                     return_summary=TRUE,
+                     prior=c('t','t','normal'),
+                     t_df=c(7,7),seed=123))
 
 })
