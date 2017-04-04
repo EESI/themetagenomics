0aba1e448011c9bc26afcddd4d3c403087bf8998
sw1 <stephenwoloszynek@gmail.com>
2017-04-04 14:45:11 -0400

    fixed the function data; added some more features to the test otu tables; added stan check

--- tests/testthat/test-function-effects.R
+++ tests/testthat/test-function-effects.R
@@ -5,3 +5,15 @@ Sys.setenv("R_TESTS" = "")
 test_that('rccp works',{
   expect_equal(example(cxxfunction, package = "inline", run.dontrun = TRUE)$value,10)
 })
+
+test_that('glmer works',{
+  DAT <- readRDS(system.file('testdata','otufuncdata.rds',package='themetagenomics'))
+  ml <- suppressWarnings(est(DAT,level=2,iters=5,method='ml'))
+  expect_is(ml,'effects')
+})
+
+test_that('stan works',{
+  DAT <- readRDS(system.file('testdata','otufuncdata.rds',package='themetagenomics'))
+  hmc <- suppressMessages(suppressWarnings(est(DAT,level=2,iters=50,chains=1,method='hmc')))
+  expect_is(ml,'effects')
+})
